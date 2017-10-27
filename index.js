a94c6af7b610dd45739a240629218d583f045df8
Kiran <akiran547@gmail.com>
2017-10-27 12:59:23 +0530

    a

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var index = require('./routes/index');
 var users = require('./routes/users');
-app.use('/e2', index);
-app.use('/e2users', users);
+module.exports = {
+	init: function function_name(app) {
+		app.use('/e2', index);
+		app.use('/e3users', users);	
+	}
+};
